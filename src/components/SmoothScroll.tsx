cd9c37597b3329dab608a5e0ca0bc79c177fded0
VR2305 <vickyravi4231@gmail.com>
2026-06-05 15:40:52 +0530

    Fix mobile scroll jank by removing syncTouch from Lenis

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "lenis/react";
 
 export const SmoothScroll = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ReactLenis root options={{ lerp: 0.05, syncTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.05 }}>
       {children}
     </ReactLenis>
   );
